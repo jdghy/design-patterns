5e64cec848bf4826116f8b7eb5b897246658121b
jayanta <jayantaduttaghy@gmail.com>
2025-06-21 15:05:09 +0530

    feat:builder pattern

---
+++ Builder/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SocialMediaPost socialMediaPost = new SocialMediaPostBuilder()
+                .AddTitle("Builder Pattern in C#")
+                .AddContent("This post explains the Builder pattern in C#.")
+                .AddAuthor("John Doe")
+                .SetCreatedAt(DateTime.Now)
+                .AddTags("C#")
+                .AddLink(new Uri("https://example.com"))
+                .AddImageUrl(new Uri("https://example.com/image.jpg"))
+                .Build();
+            Console.WriteLine(socialMediaPost.ToString());
+        }
+    }
+}
---
+++ Builder/SocialMediaPostBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class SocialMediaPostBuilder
+    {
+        private readonly SocialMediaPost _post = new SocialMediaPost();
+
+        public SocialMediaPostBuilder AddTitle(string title)
+        {
+            _post.Title = title;
+            return this;
+        }
+        public SocialMediaPostBuilder AddContent(string content)
+        {
+            _post.Content = content;
+            return this;
+        }
+        public SocialMediaPostBuilder AddAuthor(string author)
+        {
+            _post.Author = author;
+            return this;
+        }
+        public SocialMediaPostBuilder SetCreatedAt(DateTime createdAt)
+        {
+            _post.CreatedAt = createdAt;
+            return this;
+        }
+        public SocialMediaPostBuilder AddTags(string tag)
+        {
+            if(_post.Tags == null)
+            {
+                _post.Tags = new List<string>();
+            }
+            _post.Tags.Add(tag);
+            return this;
+        }
+        public SocialMediaPostBuilder AddLink(Uri link)
+        {
+            if (_post.Links == null)                                                                                                                     
+            {
+                _post.Links = new List<Uri>();
+            }
+             _post.Links.Add(link);
+             return this;
+        }
+
+        public SocialMediaPostBuilder AddImageUrl(Uri imageUri                                                       )
+        {
+            _post.ImageUri = imageUri;
+            return this;
+        }
+        public SocialMediaPost Build()
+        {
+            return _post;
+        }
+    }
+}
